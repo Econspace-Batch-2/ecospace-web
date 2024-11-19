4bf9c05db3bc7c0bd2b5de6957b44dfc2cb24b0b
gracekarinn <gracekarina12@gmail.com>
2024-11-19 18:04:55 +0700

    feat: add benefit section for tutor detail page

---
+++ resources/views/modules/tutor_detail/benefitTutor.blade.php
@@ -0,0 +1,35 @@
+<div class="border-2 rounded-lg mt-4 py-3 px-4">
+    <h3 class="text-4xl font-black text-orange-500">Benefit for You</h3>
+    
+    <div class="flex items-center gap-2 mt-4">
+        <div class="w-12 h-12">
+            <img src="{{ asset('assets/tutorDetail/benefit1.svg') }}" class="w-full h-full object-contain" alt="Expert Tutor" loading="lazy">
+        </div>
+        <span class="text-lg font-medium">1.</span>
+        <p class="text-lg font-medium">Expert Tutor yang telah diseleksi dengan kapabilitias pengetahuan yang baik</p>
+    </div>
+
+    <div class="flex items-center gap-2 mt-4">
+        <div class="w-12 h-12">
+            <img src="{{ asset('assets/tutorDetail/benefit2.svg') }}" class="w-full h-full object-contain" alt="Student System" loading="lazy">
+        </div>
+        <span class="text-lg font-medium">2.</span>
+        <p class="text-lg font-medium">Sistem Mahasiswa to Mahasiswa, sehingga bahasan materi menjadi lebih tepat & relatable</p>
+    </div>
+
+    <div class="flex items-center gap-2 mt-4">
+        <div class="w-12 h-12">
+            <img src="{{ asset('assets/tutorDetail/benefit3.svg') }}" class="w-full h-full object-contain" alt="Consultation" loading="lazy">
+        </div>
+        <span class="text-lg font-medium">3.</span>
+        <p class="text-lg font-medium">Konsultasi Materi / Tugas dan Persiapan Ujian menjadi lebih mudah bersama Tutor</p>
+    </div>
+
+    <div class="bg-blue-50 rounded-lg p-4 mt-4">
+        <p class="text-2xl font-bold mb-1">1x Tutor Session:</p>
+        <ul class="list-disc list-inside">
+            <li class="text-lg font-medium">90 Minutes (Online / Onsite)</li>
+            <li class="text-lg font-medium">Akses pada PPT Modul Subtopik</li>
+        </ul>
+    </div>
+</div>
