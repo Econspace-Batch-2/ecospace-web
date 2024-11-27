0ababc168c025e627228371e26a070c46d4935a4
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-27 18:27:22 +0700

    fix: register error

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,42 +1,59 @@
 <!-- NAVBAR -->
 <nav class="flex items-center justify-between p-4 bg-white shadow-md fixed w-full z-50">
     <div class="container mx-auto flex items-center justify-between">
+        <!-- Logo -->
         <a href="{{ url('/') }}" class="flex items-center">
             <img loading="lazy" src="{{ asset('assets/socmed/logo.svg') }}" class="h-10 md:h-20 w-auto" alt="Economic space">
         </a>
-        <button class="text-gray-500 focus:outline-none md:hidden" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+
+        <!-- Mobile Toggle Button -->
+        <button class="text-gray-500 focus:outline-none md:hidden" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
         </button>
-        <div class="hidden w-full md:flex md:items-center md:w-auto" id="navbarSupportedContent">
-            <ul class="flex flex-col md:flex-row md:space-x-4 mx-10">
+
+        <!-- Navbar Links -->
+        <div class="hidden w-full md:flex md:items-center md:w-auto" id="navbarContent">
+            <ul class="flex flex-col md:flex-row md:space-x-6">
                 <li>
-                    <a class="nav-link text-lg {{ Route::is('viewTutors') ? 'text-blue-500' : 'text-gray-700' }}" aria-current="page" href="{{url('/tutors')}}">Tutor</a>
+                    <a href="{{ url('/tutors') }}" class="text-lg {{ Route::is('viewTutors') ? 'text-blue-500' : 'text-gray-700 hover:text-blue-500' }}">
+                        Tutor
+                    </a>
                 </li>
                 <li>
-                    <a class="nav-link text-lg text-gray-700" aria-current="page" href="{{url('/soon')}}">Event</a>
+                    <a href="{{ url('/soon') }}" class="text-lg text-gray-700 hover:text-blue-500">
+                        Event
+                    </a>
                 </li>
             </ul>
-            <ul class="flex flex-col md:flex-row md:space-x-4 ml-auto">
+
+            <!-- Authentication Links -->
+            <ul class="flex flex-col md:flex-row md:space-x-6 ml-auto">
                 @guest
                     @if (Route::has('register'))
                         <li>
-                            <a class="text-lg bg-black px-4 py-2 text-white rounded-xl" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                            <a href="{{ route('register') }}" class="text-lg bg-black px-4 py-2 text-white rounded-xl hover:bg-gray-800">
+                                {{ __('Sign Up') }}
+                            </a>
                         </li>
                     @endif
                     @if (Route::has('login'))
                         <li>
-                            <a class="border-[1px] border-black text-lg px-4 py-2 rounded-xl" href="{{ route('login') }}">{{ __('Sign In') }}</a>
+                            <a href="{{ route('login') }}" class="text-lg border border-black px-4 py-2 rounded-xl hover:bg-gray-100">
+                                {{ __('Sign In') }}
+                            </a>
                         </li>
                     @endif
                 @else
-                    <li class="relative">
-                        <a id="navbarDropdown" class="nav-link text-lg text-gray-700 cursor-pointer" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <li class="relative group">
+                        <a id="navbarDropdown" class="text-lg text-gray-700 cursor-pointer hover:text-blue-500" href="#">
                             {{ Auth::user()->name }}
                         </a>
-                        <div class="dropdown-menu absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item block px-4 py-2 text-gray-700 hover:bg-gray-100" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
+                        <!-- Dropdown Menu -->
+                        <div class="hidden group-hover:block absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg">
+                            <a href="{{ route('logout') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
@@ -50,6 +67,7 @@
     </div>
 </nav>
 
-<div class="mobile bg-black text-white text-center py-2">
+<!-- Mobile Message -->
+<div class="md:hidden bg-black text-white text-center py-2">
     Open on the desktop for the best experience
 </div>
