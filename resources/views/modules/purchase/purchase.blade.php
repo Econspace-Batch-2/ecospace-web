4587bc5ae1de61fd3c0d9515a2374e6b28b24224
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-05 10:08:42 +0700

    feat: initialize bukti pembayaran section

--- resources/views/modules/purchase/purchase.blade.php
+++ resources/views/modules/purchase/purchase.blade.php
@@ -1,4 +1,6 @@
 @extends('layout.layout')
 @section('content')
-    
+    <main class="w-full h-[100vh] flex items-center justify-center">
+        @include('modules.purchase.sections.bukti_pembayaran')
+    </main>
 @endsection
