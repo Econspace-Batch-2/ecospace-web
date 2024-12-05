ddaa585051c27a47d6e72930aa6f943d3b20faae
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-05 09:48:53 +0700

    feat: footer added

--- resources/views/modules/home/home.blade.php
+++ resources/views/modules/home/home.blade.php
@@ -16,60 +16,56 @@
 
     @include('modules.home.sections.testimony')
 
-    <!-- CLOSING -->
-    <!--desktop-->
     <div class="desktop">
-        <div class="container-fluid bg-img-blue-square mt-5">
-            <div class="px-md-5 py-md-5"
+        <div class="bg-img-blue-square mt-5 bg-cover min-h-screen relative">
+            <div
                 style="background: url('assets/background/assetbgblue.svg');background-repeat: no-repeat;background-size: cover;z-index:0px;padding-top:0px;padding-bottom:100px;height:100%">
-                <div class="wrapper">
-                    <div class="p"></div>
-                    <div class="img-area">
-                        <a href="https://www.linkedin.com/company/economic-space/mycompany/" target="_blank"
-                            aria-label="go to economic space linkedin">
-                            <img loading="lazy" src="{{ asset('assets/home/sheet1.svg') }}"
-                                class="img img-box box-4 hovered" alt="linkedin">
-                        </a>
-                        <a href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
-                            aria-label="economic space whatsapp number">
-                            <img loading="lazy" src="{{ asset('assets/home/sheet2.svg') }}"
-                                class="img img-box box-5 hovered" alt="whatsapp">
-                        </a>
-                        <img loading="lazy" src="{{ asset('assets/home/sheet3.svg') }}" class="img img-box box-6 hovered"
-                            alt="website">
+                <div class="img-area absolute top-0 left-0 translate-x-[50%] translate-y-[50%] xl:scale-150">
+                    <a href="https://www.linkedin.com/company/economic-space/mycompany/" target="_blank"
+                        aria-label="go to economic space linkedin">
+                        <img loading="lazy" src="{{ asset('assets/home/sheet1.svg') }}" class="img img-box box-4 hovered"
+                            alt="linkedin">
+                    </a>
+                    <a href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
+                        aria-label="economic space whatsapp number">
+                        <img loading="lazy" src="{{ asset('assets/home/sheet2.svg') }}" class="img img-box box-5 hovered"
+                            alt="whatsapp">
+                    </a>
+                    <img loading="lazy" src="{{ asset('assets/home/sheet3.svg') }}" class="img img-box box-6 hovered"
+                        alt="website">
 
 
-                        <a href="https://www.linkedin.com/company/economic-space/mycompany/" target="_blank"
-                            aria-label="go to economic space linkedin">
-                            <img loading="lazy" src="{{ asset('assets/home/folder1.svg') }}"
-                                class="img img-box box-7 hovered" alt="linkedin">
-                        </a>
-                        <a href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
-                            aria-label="economic space whatsapp number">
-                            <img loading="lazy" src="{{ asset('assets/home/folder2.svg') }}"
-                                class="img img-box box-8 hovered" alt="whatsapp">
-                        </a>
-                        <img loading="lazy" src="{{ asset('assets/home/folder3.svg') }}" class="img img-box box-9 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-10 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-11 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-12 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/ppt2.svg') }}" class="img img-box box-1 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/ppt3.svg') }}" class="img img-box box-2 hovered"
-                            alt="decorative image">
-                        <img loading="lazy" src="{{ asset('assets/home/ppt1.svg') }}" class="img img-box box-3 hovered"
-                            alt="decorative image">
-                    </div>
+                    <a href="https://www.linkedin.com/company/economic-space/mycompany/" target="_blank"
+                        aria-label="go to economic space linkedin">
+                        <img loading="lazy" src="{{ asset('assets/home/folder1.svg') }}" class="img img-box box-7 hovered"
+                            alt="linkedin">
+                    </a>
+                    <a href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
+                        aria-label="economic space whatsapp number">
+                        <img loading="lazy" src="{{ asset('assets/home/folder2.svg') }}"
+                            class="img img-box box-8 hovered" alt="whatsapp">
+                    </a>
+                    <img loading="lazy" src="{{ asset('assets/home/folder3.svg') }}" class="img img-box box-9 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-10 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-11 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/arrow.svg') }}" class="img img-box box-12 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/ppt2.svg') }}" class="img img-box box-1 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/ppt3.svg') }}" class="img img-box box-2 hovered"
+                        alt="decorative image">
+                    <img loading="lazy" src="{{ asset('assets/home/ppt1.svg') }}" class="img img-box box-3 hovered"
+                        alt="decorative image">
                 </div>
-                <div class="overlay-text">
-                    <div class="col-10 ">
-                        <h5 class="font-36 text-orange "><span class="font-family font-900 bg-white text-center">
-                                Memberikan Layanan</span> </h5>
-                        <h5 class="font-36 text-orange "><span class="font-family font-900  bg-white text-center">End to
+                <div
+                    class="relative top-[15%] left-[22%] -translate-x-[50%] -translate-y-[50%] text-left p-[20px] w-[80%] max-w-[525px]">
+                    <div class="col-10">
+                        <h5 class="font-36 text-orange "><span
+                                class="font-family font-900 bg-white text-center">Memberikan Layanan</span> </h5>
+                        <h5 class="font-36 text-orange "><span class="font-family font-900 bg-white text-center">End to
                                 End untuk</span> </h5>
                         <h5 class="font-36 text-orange "><span
                                 class="font-family font-900 bg-white text-center">Pengembangan Dirimu</span></h5>
@@ -79,17 +75,17 @@
         </div>
     </div>
 
-    <div class="desktop">
-        <div class="container">
-            <div class="box-orange d-flex justify-content-between">
-                <div class="box-wa-1 flex-fill">
+    <div class="desktop w-full">
+        <div class="container mx-auto">
+            <div class="box-orange flex flex-row items-center">
+                <div class="box-wa-1 flex-grow">
                     <h3 class="text-white font-700 font-36 mb-0 mt-2">Punya pertanyaan lebih lanjut?</h3>
                 </div>
-                <div class="box-wa justify-content-center hovered">
+                <div class="box-wa">
                     <img loading="lazy" class="img-fluid icon-wa" src="{{ asset('assets/socmed/wa.svg') }}"
                         alt="icon whatsapp">
-                    <div class="font-400 font-24">
-                        <a href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
+                    <div>
+                        <a class="text-black" href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
                             aria-label="economic space whatsapp number">
                             Hubungi Kami
                         </a>
@@ -100,17 +96,16 @@
     </div>
     </div>
 
-
     {{-- KOTAK BIRU YANG MOBILE --}}
     <div class="mobile">
         <div class="container-fluid bg-img-blue-square justify-content-center">
             <div class="px-md-5 py-md-5"
                 style="background: url('');background-repeat: no-repeat;background-size: cover;z-index:0px;padding-top:0px;padding-bottom:100px;height:100%">
                 <div class="overlay-text">
-                    <h5 class="text-orange "><span class="font-48 font-family font-900 bg-white text-center">
-                            Memberikan Layanan</span> </h5>
-                    <h5 class="text-orange "><span class="font-48 font-family font-900  bg-white text-center">End to
-                            End untuk</span> </h5>
+                    <h5 class="text-orange "><span class="font-48 font-family font-900 bg-white text-center"> Memberikan
+                            Layanan</span> </h5>
+                    <h5 class="text-orange "><span class="font-48 font-family font-900  bg-white text-center">End to End
+                            untuk</span> </h5>
                     <h5 class="text-orange "><span class="font-48 font-family font-900 bg-white text-center">Pengembangan
                             Dirimu</span></h5>
                 </div>
@@ -120,20 +115,19 @@
                             <img loading="lazy" src="{{ asset('assets/home/icon2.svg') }}" class="icon-1 m-1"
                                 alt="whatsapp icon" style="width: 50px;">
                             <h3 class="font-900 font-32 mt-1 text-right">WhatsApp Community</h3>
-                            <p class="font-18 text-right mt-1">Open Space Community to learn insightful thing about
-                                College & Professional Life</p>
+                            <p class="font-18 text-right mt-1">Open Space Community to learn insightful thing about College
+                                & Professional Life</p>
                             <button class="btn btn-danger font-700 font-24   mt-1"><a
                                     href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW" target="_blank"
-                                    class="text-white" aria-label="economic space whatsapp number">Join
-                                    Now</a></button>
+                                    class="text-white" aria-label="economic space whatsapp number">Join Now</a></button>
                         </div>
                         <div class="card card-2">
                             <img loading="lazy" src="{{ asset('assets/home/icon1.svg') }}" class="icon-2 m-1"
                                 alt="whatsapp icon" style="width: 50px;">
                             <h3 class="font-900 font-32 mt-1 text-right" style="text-align: right;">LinkedIn Career &
                                 Competition</h3>
-                            <p class="font-18 text-right mt-1" style="text-align: right;">Exclusive information on
-                                Career, Scholarship, Competition, etc. No More Miss Out on Opportunities</p>
+                            <p class="font-18 text-right mt-1" style="text-align: right;">Exclusive information on Career,
+                                Scholarship, Competition, etc. No More Miss Out on Opportunities</p>
                             <button class="btn btn-danger font-700 font-24 mt-1"><a
                                     href="https://www.linkedin.com/company/economic-space/mycompany/" target="_blank"
                                     class="text-white" aria-label="go to economic space linkedin">Discover
@@ -148,16 +142,16 @@
         {{-- kotak oren --}}
 
         <div class="mobile">
-            <div class="container">
-                <div class="box-orange">
+            <div class="container mx-auto">
+                <div class="box-orange pt-4 flex flex-col items-center">
                     <div class="text-center">
                         <p class="font-36 font-700 mb-1">Punya pertanyaan lebih lanjut?</p>
                     </div>
-                    <div class="box-wa justify-content-center mb-1 mt-0">
+                    <div class="box-wa mb-1 mt-0">
                         <img loading="lazy" class="icon-wa" src="{{ asset('assets/socmed/wa.svg') }}"
                             alt="whatsapp icon">
                         <a target="_blank" href="https://chat.whatsapp.com/IDInUpM1avX7ogb091PLnW"
-                            aria-label="economic space whatsapp number">
+                            aria-label="economic space whatsapp number" class="text-black">
                             Hubungi Kami
                         </a>
                     </div>
@@ -165,8 +159,6 @@
             </div>
         </div>
     </div>
-
-    {{-- FOOTER --}}
 
 @section('desktopBtn')
     Daftar Kelas
